992fa531078b8d8fb3119e0bdccc17104dc35846
Melaton <contact@melaton.top>
2024-05-28 10:27:57 +0800

    Improve code

--- tml.sh
+++ tml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-tml_dir=/root/.local/share/Terraria/
-tmp_dir=/root/.local/share/Terraria/Temp/
+tmodloader_directory=${TMODLOADER_DIRECTORY:-/root/.local/share/Terraria/}
+temp_directory=${TEMP_DIRECTORY:-/root/.local/share/Terraria/Temp/}
 release_url=$(curl -s https://api.github.com/repos/tModLoader/tModLoader/releases/latest | grep browser_download_url | grep tModLoader.zip | cut -d '"' -f 4)
 
 # Menu/Color inspired by https://github.com/TheyCallMeSecond/sing-box-manager
@@ -12,24 +12,24 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 
 # 1 Start tModLoader
-function start_server() {
+start_server() {
     clear
-    local is_exist=false
+    local is_configured=false
     # Check if tModLoader installed
-    if [ -f "$tml_dir/start-tModLoaderServer.sh" ]; then
+    if [ -f "$tmodloader_directory/start-tModLoaderServer.sh" ]; then
         # Check if "tml-manager" screen session exists
         if screen -S tml-manager -Q select >/dev/null; then
             # Check if serverconfig.txt is modified
             while IFS= read -r line; do
                 # Use "#port=" to judge
                 if [[ "$line" == *"#port="* ]]; then
-                    is_exist=true
-                    echo exist
+                    is_configured=true
                     break
                 fi
-            done < "$tml_dir/serverconfig.txt"
-            if [ -e "is_exist" ]; then
-                read -p "$(echo -e "${CYAN}You haven't configure tModLoader, press enter to configure. ${NC}")"
+            done < "$tmodloader_directory/serverconfig.txt"
+            if ! $is_configured; then
+                echo -e "${CYAN}You haven't configured tModLoader, press enter to configure. ${NC}"
+                read
                 modify_config
                 start_server
             else
@@ -38,50 +38,54 @@ function start_server() {
         else
             echo -e "${CYAN}Initialize session first${NC}"
             screen -dmS tml-manager
-            read -p "$(echo -e "${CYAN}Initialized successfully! Press enter to continue. ${NC}")"
+            echo -e "${CYAN}Initialized successfully! Press enter to continue. ${NC}"
+            read
             start_server
         fi
     else
-        read -p "$(echo -e "${CYAN}You haven't installed tModLoader, press enter to install. ${NC}")"
+        echo -e "${CYAN}You haven't installed tModLoader, press enter to install. ${NC}"
+        read
         update_tml
     fi
 }
 
 # 2 Stop tModLoader
-function stop_server() {
+stop_server() {
     clear
     sleep 1
     screen -S tml-manager -p 0 -X stuff 'exit\n'
     sleep 1
-    read -p "$(echo -e "${CYAN}Server stopped successfully! Press enter to continue. ${NC}")"
+    echo -e "${CYAN}Server stopped successfully! Press enter to continue. ${NC}"
+    read
     main_menu
 }
 
 # 3 Update tModLoader
-function update_tml() {
+update_tml() {
     clear
-    rm "$tmp_dir/tModLoader.zip" >/dev/null
-    wget -P "$tmp_dir" "${release_url}" >/dev/null
-    unzip -d "$tml_dir" -o "$tmp_dir/tModLoader.zip" -x "serverconfig.txt" >/dev/null
-    read -p "$(echo -e "${CYAN}Install successfully! Press enter to continue. ${NC}")"
+    rm -f "$temp_directory/tModLoader.zip"
+    wget -P "$temp_directory" "${release_url}" -q
+    unzip -d "$tmodloader_directory" -o "$temp_directory/tModLoader.zip" -x "serverconfig.txt" -q
+    echo -e "${CYAN}Install successfully! Press enter to continue. ${NC}"
+    read
     main_menu
 }
 
 # 4 Modify configs
-function modify_config() {
+modify_config() {
     clear
     read -p "Maximum number of players: " v_max
     read -p "Port of server: " v_port
     read -p "Password of server(Press enter to none): " v_pass
 
     # Modify server configs
-    sed -i "/^#*maxplayers/c\maxplayers=$v_max" "$tml_dir/serverconfig.txt"
-    sed -i "/^#*port/c\port=$v_port" "$tml_dir/serverconfig.txt"
-    sed -i "/^#*password/c\password=$v_pass" "$tml_dir/serverconfig.txt"
+    sed -i "/^#*maxplayers/c\maxplayers=$v_max" "$tmodloader_directory/serverconfig.txt"
+    sed -i "/^#*port/c\port=$v_port" "$tmodloader_directory/serverconfig.txt"
+    sed -i "/^#*password/c\password=$v_pass" "$tmodloader_directory/serverconfig.txt"
 }
 
 # 5 Manage Worlds
-function manage_worlds() {
+manage_worlds() {
     clear
     local choice
     echo "╔════════════════════════════════════════════════════════════════════════╗"
@@ -99,8 +103,9 @@ function manage_worlds() {
         echo -e "Your Worlds:" 
         echo -e "${CYAN}$(find "/root/.local/share/Terraria/tModLoader/Worlds" -type f -name "*.wld" -exec basename {} .wld \;)${NC}"
         read -p "Type in the name of the world you want to play: " v_world
-        sed -i "/^#*world/c\world="$tml_dir"tModLoader/Worlds/"$v_world".wld" "$tml_dir/serverconfig.txt"
-        read -p "$(echo -e "${CYAN}Changed successfully! Press enter to back to Menu. ${NC}")"
+        sed -i "/^#*world/c\world="$tmodloader_directory"/tModLoader/Worlds/"$v_world".wld" "$tmodloader_directory/serverconfig.txt"
+        echo -e "${CYAN}Changed successfully! Press enter to back to Menu. ${NC}"
+        read
         main_menu
         ;;
     2)
@@ -113,31 +118,29 @@ function manage_worlds() {
 }
 
 # 6 Update script
-function update_script () {
-    echo "$1" # arguments are accessible through $1, $2,...
+update_script () {
+    echo "working in progress"
 }
 
+launch_server() {
 
-function launch_server() {
+    local port=$(grep "port=" $tmodloader_directory/serverconfig.txt | cut -d'=' -f2)
 
-    local port=$(grep "port=" $tml_dir/serverconfig.txt | cut -d'=' -f2)
-
-    chmod +x $tml_dir/LaunchUtils/ScriptCaller.sh
-    # TODO: replace with $tml_dir
+    chmod +x $tmodloader_directory/LaunchUtils/ScriptCaller.sh
     sleep 1
     screen -S tml-manager -p 0 -X stuff 'cd /root/.local/share/Terraria/\n'
     screen -S tml-manager -p 0 -X stuff 'bash /root/.local/share/Terraria/LaunchUtils/ScriptCaller.sh -server -config serverconfig.txt\n'
     sleep 1
-    # Waiting for $port to open
     echo "Starting..."
     while ! nc -z localhost $port; do
         sleep 1
     done
-    read -p "$(echo -e "${CYAN}Server started! Press enter to back to Menu. ${NC}")"
+    echo -e "${CYAN}Server started! Press enter to back to Menu. ${NC}"
+    read
     main_menu
 }
 
-function main_menu() {
+main_menu() {
     clear
     echo "╔════════════════════════════════════════════════════════════════════════╗"
     echo -e "║ ${YELLOW}                     ████████╗███╗   ███╗██╗     ${NC}                      ║"
